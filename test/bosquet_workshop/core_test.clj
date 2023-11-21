f2e6e5f90959657fee828691bf3024b081cdd773
Jason Walsh <j@wal.sh>
2023-11-21 06:46:44 -0800

    Workshop

---
+++ test/bosquet_workshop/core_test.clj
@@ -0,0 +1,7 @@
+(ns bosquet-workshop.core-test
+  (:require [clojure.test :refer :all]
+            [bosquet-workshop.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
